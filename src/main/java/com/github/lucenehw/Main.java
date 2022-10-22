e39e75c5b29f48dbdd08860190675370a042d72f
Tibbo93 <alessandrotibaldi93@gmail.com>
2022-10-22 19:34:27 +0200

    Add execution times in Main class

--- src/main/java/com/github/lucenehw/Main.java
+++ src/main/java/com/github/lucenehw/Main.java
@@ -9,7 +9,10 @@ public class Main {
     public static void main(String[] args) {
         try {
             Indexer indexer = new Indexer("lucene-index", "documents");
+            long start = System.nanoTime();
             indexer.createIndex();
+            long end = System.nanoTime();
+            System.out.println("Indexing time: " + ((end - start)/1000000) + "ms");
 
             Scanner scanner = new Scanner(System.in);
             System.out.println("\nFields allowed: 'titolo', 'contenuto");
@@ -19,7 +22,10 @@ public class Main {
             String query = scanner.nextLine();
 
             Searcher searcher = new Searcher("lucene-index");
+            start = System.nanoTime();
             searcher.search(query);
+            end = System.nanoTime();
+            System.out.println("Searching time: " + ((end - start)/1000000) + "ms");
             searcher.close();
         } catch (IOException | ParseException e) {
             System.out.println("Error: " + e.getMessage());
